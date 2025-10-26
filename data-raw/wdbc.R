2c81e2d5190fbefe7633236f07cf17f6d93f76a4
AneeshAgarwala <aaga0022@student.monash.edu>
2025-10-26 13:48:00 +1100

    Document Lazy Data

--- data-raw/wdbc.R
+++ data-raw/wdbc.R
@@ -1,13 +1,3 @@
-#' Wisconsin Diagnostic Breast Cancer
-#'
-#' Created by Dr. William H. Wolberg, W. Nick Street, and Olvi L. Mangasarian at the University of Wisconsin–Madison
-#' to classify breast tumor samples as benign or malignant based on quantitative image analysis of fine needle aspirate \(FNA \) images of breast masses.
-#'
-#' @format ## `wdbc`
-#' A data frame with 569 rows and 32 columns
-#'
-#' @source <https://archive.ics.uci.edu/dataset/17/breast+cancer+wisconsin+diagnostic>
-
 wdbc <- read.csv("D:/SVMODT/project-svodt/data-raw/wdbc.csv", header=TRUE)
 wdbc <- wdbc[,2:32]
 wdbc$diagnosis <- as.factor(wdbc$diagnosis)
