f732b9ad285f6229f34a1c6d2030f2380ac64868
AneeshAgarwala <aaga0022@student.monash.edu>
2026-04-06 16:32:29 +1000

    Add Wine Lazy Data
    Multiclass Vignette Classification Examples

---
+++ R/wine.R
@@ -0,0 +1,31 @@
+#' Wine Dataset
+#'
+#' The Wine dataset contains the results of a chemical analysis of wines
+#' derived from three different cultivars grown in the same region of Italy.
+#' The dataset is commonly used for multiclass classification tasks, where
+#' the objective is to identify the cultivar of origin based on physicochemical
+#' properties.
+#'
+#' @format A data frame with 178 rows and 14 columns:
+#' \describe{
+#'    \item{class}{Factor with levels 1, 2, and 3 indicating cultivar}
+#'    \item{alcohol}{Alcohol content}
+#'    \item{malic_acid}{Malic acid concentration}
+#'    \item{ash}{Ash content}
+#'    \item{alcalinity_of_ash}{Alcalinity of ash}
+#'    \item{magnesium}{Magnesium content}
+#'    \item{total_phenols}{Total phenols}
+#'    \item{flavanoids}{Flavonoid content}
+#'    \item{nonflavanoid_phenols}{Nonflavanoid phenols}
+#'    \item{proanthocyanins}{Proanthocyanin content}
+#'    \item{color_intensity}{Color intensity}
+#'    \item{hue}{Hue}
+#'    \item{od280_od315}{OD280/OD315 of diluted wines}
+#'    \item{proline}{Proline concentration}
+#'
+#' }
+#'
+#' @source
+#' Aeberhard, S. & Forina, M. (1992). Wine Dataset. UCI Machine Learning Repository.
+#' Original dataset available at: <https://archive.ics.uci.edu/dataset/109/wine>
+"wine"
